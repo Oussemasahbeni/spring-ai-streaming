463145ed008333a5f2881008ceb267635619a877
oussema sahbeni <oussemasahbeni300@gmail.com>
2025-05-12 15:34:07 +0100

    initial commit

---
+++ src/main/java/com/oussema/streaming/ChatController.java
@@ -0,0 +1,35 @@
+package com.oussema.streaming;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ChatController {
+
+    private final  ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+
+    }
+
+    @PostMapping("/chat")
+    public String chat(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> chatWithStream(@RequestParam String message) {
+        return chatClient
+                .prompt(message)
+                .stream()
+                .content();
+    }
+}
---
+++ src/test/java/com/oussema/streaming/StreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oussema.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
